f02e995060e5859a0897de264f79c876fe6a1bdf
Thanika <90124926+Thanika1979@users.noreply.github.com>
2021-12-21 15:54:38 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//Case 1
+var Name = 'triangleOne';
+triangleOne = (10 * 13) /2;
+var Name = 'triangleTwo';
+triangleTwo = (16.5 * 20.3) /2;
+
+//Case 2
+var Name = 'triangleThree';
+triangleThree = (16.5 * 20.3) /2;
+var Name = 'triangleFour'
+triangleFour = (20.3 * 16.5) /2;
+
+//Case 3
+var Name = 'triangleFive';
+triangleFive = (7.8 * 5.6) /2;
+var Name = 'triangleSix';
+triangleSix = (9.3 * 12.4) /2;
+
+
+
+var baseValue = prompt ('Enter the base of Name: ') ;
+var hightValue = prompt ('Enter the height of a Name: ') ;
+
+//calculate the area
+var areaValue = (baseValue * hightValue) /2 ;
+
+console.log (Name)
+console.log (areaValue)
+
+//Area of the triangle
+//triangleOne = 65 cm2.
+//triangleTwo = 167.475 cm2.
+//triangleThree = 167.475 cm2
+//trianglgFour = 167.475 cm2.
+//triangleFive = 21.84 cm2.
+//triangleSix = 57.66 cm2.
+ //The biggest Triangle is triangleTwo, triangleThree, triangleFour.
+
+ //OBS!In JavaScript, the functions wrapped with parenthesis for wraps it code inside a function scope and decrease clashing with other libraries.
